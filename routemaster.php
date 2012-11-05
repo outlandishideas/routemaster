af6e305d273bf9773b35264dcd2e2138b83652e6
Tamlyn Rhodes <tamlyn@tamlyn.org>
2012-11-05 13:58:39 +0000

    Check for index-rm.php on permalinks flush

--- routemaster.php
+++ routemaster.php
@@ -10,10 +10,8 @@ require_once 'RoutemasterException.class.php';
 
 register_activation_hook(__FILE__, function() {
 	global $wp_rewrite;
-	//rewrite .htaccess
+	//rewrite .htaccess (and copy index file)
 	$wp_rewrite->flush_rules(true);
-	//copy alternate index file
-	copy(dirname(__FILE__).'/index-rm.php', ABSPATH . 'index-rm.php');
 });
 
 register_deactivation_hook(__FILE__, function() {
@@ -28,7 +26,14 @@ register_deactivation_hook(__FILE__, function() {
 //rewrite htaccess file to route via index-rm.php
 add_filter('mod_rewrite_rules', 'rm_mod_rewrite_rules');
 function rm_mod_rewrite_rules($rules) {
+	//rewrite rewrite rules
 	$rules = "DirectoryIndex index-rm.php index.php\n" . str_replace('index', 'index-rm', $rules);
 //	$rules = preg_replace('%([\w/-]*)index.php \[L\]%', "$1index-rm.php [L]\nRewriteRule ^$ $1index-rm.php", $rules);
+
+	//copy alternate index file
+	if (!file_exists(ABSPATH . 'index-rm.php')) {
+		copy(dirname(__FILE__) . '/index-rm.php', ABSPATH . 'index-rm.php');
+	}
+
 	return $rules;
 }
