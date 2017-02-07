32abfa4389dcc965c47ed182636abc546ea0839f
Rasmus Winter <rasmus@outlandish.com>
2017-02-07 09:35:05 +0000

    Adding isDelete function

--- src/RouterHelper.php
+++ src/RouterHelper.php
@@ -6,30 +6,39 @@ use Outlandish\Wordpress\Routemaster\Response\HtmlResponse;
 
 class RouterHelper
 {
-	/**
-	 * Routes use this when creating a response
-	 * @param array|object $args
-	 * @return HtmlResponse
-	 */
-	public function createDefaultResponse($args = [])
-	{
-		return new HtmlResponse($args);
-	}
+    /**
+     * Routes use this when creating a response
+     * @param array|object $args
+     * @return HtmlResponse
+     */
+    public function createDefaultResponse($args = [])
+    {
+        return new HtmlResponse($args);
+    }
 
-	public function isPost() {
-		return strtoupper($_SERVER['REQUEST_METHOD']) == 'POST';
-	}
+    public function getRequestMethod()
+    {
+        return strtoupper($_SERVER['REQUEST_METHOD']);
+    }
 
-	public function isGet() {
-		return strtoupper($_SERVER['REQUEST_METHOD']) == 'GET';
-	}
+    public function isPost() {
+        return $this->getRequestMethod() == 'POST';
+    }
 
-	public function isPut() {
-		return strtoupper($_SERVER['REQUEST_METHOD']) == 'PUT';
-	}
+    public function isGet() {
+        return $this->getRequestMethod() == 'GET';
+    }
 
-	public function getRequestBody() {
-		$body = file_get_contents('php://input');
-		return $body ? json_decode($body) : null;
-	}
+    public function isPut() {
+        return $this->getRequestMethod() == 'PUT';
+    }
+
+    public function isDelete() {
+        return $this->getRequestMethod() == 'DELETE';
+    }
+
+    public function getRequestBody() {
+        $body = file_get_contents('php://input');
+        return $body ? json_decode($body) : null;
+    }
 }
