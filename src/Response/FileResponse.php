a6cbcba44c9e40683d4aed4c15d2598cd09a535b
Rasmus Winter <rasmus@outlandish.com>
2017-03-29 18:05:46 +0100

    Making FileResponse more configurable

--- src/Response/FileResponse.php
+++ src/Response/FileResponse.php
@@ -4,28 +4,37 @@ namespace Outlandish\Wordpress\Routemaster\Response;
 
 class FileResponse extends RoutemasterResponse
 {
-	protected $file;
+    protected $file;
+    public $deleteOnSent = false;
 
-	public function __construct($filePath, $fileName, $disableCache = false)
-	{
-		parent::__construct([]);
+    public function __construct($filePath, $fileName, $contentType, $asAttachment = true, $enableBrowserCache = false)
+    {
+        parent::__construct([]);
 
-		if (substr($filePath, -4) == '.pdf') {
-			$this->headers[] = 'Content-Type: application/pdf';
-		}
-		if ($disableCache) {
-			$this->headers[] = "Cache-Control: no-cache, no-store, must-revalidate"; // HTTP 1.1
-			$this->headers[] = "Pragma: no-cache"; // HTTP 1.0
-			$this->headers[] = "Expires: 0"; // Proxies
-		}
-		$this->headers[] = 'Content-Disposition: inline; filename=' . $fileName;
-		$this->file = $filePath;
-	}
+        $this->headers[] = 'Content-Type: ' . $contentType;
 
-	protected function render()
-	{
-		readfile($this->file);
-	}
+        if (!$enableBrowserCache) {
+            $this->headers[] = "Cache-Control: no-cache, no-store, must-revalidate"; // HTTP 1.1
+            $this->headers[] = "Pragma: no-cache"; // HTTP 1.0
+            $this->headers[] = "Expires: 0"; // Proxies
+        }
 
+        $this->headers[] = 'Content-Disposition: ' . ($asAttachment ? 'attachment' : 'inline') . '; filename=' . $fileName;
 
+        $this->file = $filePath;
+    }
+
+    protected function render()
+    {
+        readfile($this->file);
+    }
+
+    protected function postRender()
+    {
+        parent::postRender();
+
+        if ($this->deleteOnSent) {
+            unlink($this->file);
+        }
+    }
 }
