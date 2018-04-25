49f58881c6ff7ffe713cbe4ce2703fa5de91eeca
harry <harry@outlandish.com>
2018-04-25 14:55:16 +0100

    Stopped relying on ['scheme'] which turns out not to be reliable

--- src/Router.php
+++ src/Router.php
@@ -41,7 +41,7 @@ abstract class Router
 	 */
 	public function setup()
 	{
-        $requestUrl = $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+        $requestUrl = (isset($_SERVER['HTTPS']) ? "https" : "http")  . "://" . $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
         $requestPath = str_replace(home_url(), "", $requestUrl);
         $isJsonRequest =  strpos($requestPath, '/wp-json') === 0 ;
 
