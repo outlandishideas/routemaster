20c72dfe77a6b69e0e03717de74cdc9edfb60638
Rasmus Winter <rasmus@outlandish.com>
2020-02-25 10:33:11 +0000

    Adding custom preview route

--- src/Oowp/OowpRouter.php
+++ src/Oowp/OowpRouter.php
@@ -12,84 +12,116 @@ use Outlandish\Wordpress\Routemaster\Router;
 /**
  * An OOWP-aware router
  */
+
 /** @property OowpRouterHelper $helper */
-abstract class OowpRouter extends Router {
+abstract class OowpRouter extends Router
+{
 
-	protected function __construct($helper = null) {
-		parent::__construct($helper ?: new OowpRouterHelper());
-		add_filter('post_type_link', function($postLink, $post, $leavename, $sample) {
-			return $this->permalinkHook($postLink, $post, $leavename);
-		}, 10, 4);
-	}
+    protected function __construct($helper = null)
+    {
+        parent::__construct($helper ?: new OowpRouterHelper());
+        add_filter('post_type_link', function ($postLink, $post, $leavename, $sample) {
+            return $this->permalinkHook($postLink, $post, $leavename);
+        }, 10, 4);
+    }
 
-	protected function getDefaultRoutePatterns()
-	{
-		return array_merge(parent::getDefaultRoutePatterns(), [
-			'|^sitemap.xml$|i' => 'sitemap', //xml sitemap for google etc
-			'|([^/]+)/?$|' => 'defaultPost', //matches blah/blah/slug
-			'|^$|' => 'frontPage' //matches empty string
-		]);
-	}
+    protected function getDefaultRoutePatterns()
+    {
+        return array_merge(parent::getDefaultRoutePatterns(), [
+            '|^sitemap.xml$|i' => 'sitemap', //xml sitemap for google etc
+            '|^__preview__/([^/]+)/(\d+)/?$|' => 'previewPost', //__preview_/{post type}/{post id}
+            '|([^/]+)/?$|' => 'defaultPost', //matches blah/blah/slug
+            '|^$|' => 'frontPage' //matches empty string
+        ]);
+    }
 
 
-	/** @var null Used in permalinkHook function, to prevent infinite recursion */
-	protected $permalinkHookPostId = null;
+    /** @var null Used in permalinkHook function, to prevent infinite recursion */
+    protected $permalinkHookPostId = null;
 
-	/**
-	 * Overwrites the post_link with the post's permalink()
-	 * @param $post_link
-	 * @param $post
-	 * @return string|void
-	 */
-	public function permalinkHook($post_link, $post, $leaveName) {
-		if ($post->post_name && $post->ID != $this->permalinkHookPostId) {
-			// prevent infinite recursion by saving the ID before calling permalink() (which may come back here again)
-			$this->permalinkHookPostId = $post->ID;
-			$post_link = WordpressPost::createWordpressPost($post)->permalink($leaveName);
-			$this->permalinkHookPostId = null;
-		}
-		return $post_link;
-	}
+    /**
+     * Overwrites the post_link with the post's permalink()
+     * @param string $post_link
+     * @param \WP_Post $post
+     * @param boolean $leaveName
+     * @return string|void
+     */
+    public function permalinkHook($post_link, $post, $leaveName)
+    {
+        if ($post->post_name) {
+            if ($post->ID != $this->permalinkHookPostId) {
+                // prevent infinite recursion by saving the ID before calling permalink() (which may come back here again)
+                $this->permalinkHookPostId = $post->ID;
+                $post_link                 = WordpressPost::createWordpressPost($post)->permalink($leaveName);
+                $this->permalinkHookPostId = null;
+            }
+        } elseif (in_array($post->post_status, ['draft', 'auto-draft', 'inherit'])) {
+            $post_link = get_bloginfo('url') . '/__preview__/' . $post->post_type . '/' . $post->ID;
+        }
+        return $post_link;
+    }
 
-	/***********************************************
-	 *
-	 *  Methods for default routes (defined above)
-	 *
-	 ***********************************************/
+    /***********************************************
+     *
+     *  Methods for default routes (defined above)
+     *
+     ***********************************************/
 
-	/**
-	 * @route /sitemap.xml
-	 */
-	protected function sitemap() {
-	    $view = new SitemapView(new OowpQuery(array('post_type' => 'any', 'orderby' => 'date')));
-		return new XmlResponse($view);
-	}
+    /**
+     * @route /sitemap.xml
+     */
+    protected function sitemap()
+    {
+        $view = new SitemapView(new OowpQuery(array('post_type' => 'any', 'orderby' => 'date')));
+        return new XmlResponse($view);
+    }
 
-	/**
-	 * @route /any/unknown/route
-	 */
-	protected function show404() {
-		global $post;
-		$post = new FakePost(array('post_title' => 'Page not found'));
-		return parent::show404();
-	}
+    /**
+     * @route /any/unknown/route
+     */
+    protected function show404()
+    {
+        global $post;
+        $post = new FakePost(array('post_title' => 'Page not found'));
+        return parent::show404();
+    }
 
-	/**
-	 * @route /default/route/when/no/other/match
-	 */
-	protected function defaultPost($slug) {
-		return [
-			'post' => $this->helper->querySingle(array('name' => $slug, 'post_type' => 'any'), true)
-		];
-	}
+    /**
+     * @route /default/route/when/no/other/match
+     * @param string $slug
+     * @return array
+     */
+    protected function defaultPost($slug)
+    {
+        $args = ['name' => $slug, 'post_type' => 'any'];
+        return [
+            'post' => $this->helper->querySingle($args, true)
+        ];
+    }
 
-	/**
-	 * @route /
-	 */
-	protected function frontPage() {
-		return [
-			'post' => $this->helper->querySingle(array('page_id' => get_option('page_on_front')), true)
-		];
-	}
+    /**
+     * @route /__preview__/{post type}/{post id}
+     * @param string $postType
+     * @param string $id
+     * @return array
+     */
+    protected function previewPost($postType, $id)
+    {
+        $args = ['id' => $id, 'post_type' => $postType]; // querySingle will apply the correct post status parameters
+        return [
+            'post' => $this->helper->querySingle($args, false)
+        ];
+    }
+
+    /**
+     * @route /
+     */
+    protected function frontPage()
+    {
+        $args = ['page_id' => get_option('page_on_front')];
+        return [
+            'post' => $this->helper->querySingle($args, true)
+        ];
+    }
 
 }
